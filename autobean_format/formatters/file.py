b5cffa326498b00c46beb366202097e236ad0c7f
SEIAROTg <seiarotg@gmail.com>
2023-03-29 22:42:49 +0100

    refactor: refactor file formatter.

--- autobean_format/formatters/file.py
+++ autobean_format/formatters/file.py
@@ -3,6 +3,7 @@ from autobean_refactor import models
 from . import base
 
 _DirectiveOrComment: TypeAlias = models.Directive | models.BlockComment
+_Block: TypeAlias = list[_DirectiveOrComment]
 _BLANK_LINE_SURROUNDED = {
     models.BlockComment,
     models.IgnoredLine,
@@ -20,34 +21,51 @@ def _get_category(model: _DirectiveOrComment) -> str:
     return 'other'
 
 
-def _get_spacing(prev: Optional[_DirectiveOrComment], next: _DirectiveOrComment) -> str:
+def _should_split(prev: Optional[_DirectiveOrComment], current: _DirectiveOrComment) -> bool:
     if prev is None:
-        return ''
-    
+        return False
     current_spacing = prev.spacing_after.count('\n')
     if current_spacing >= 2:
-        # If multiple blank lines are needed, maybe there should be a block comment.
-        return '\n\n'
-    if type(prev) in _BLANK_LINE_SURROUNDED or type(next) in _BLANK_LINE_SURROUNDED:
-        return '\n\n'
-    if _get_category(prev) != _get_category(next):
-        return '\n\n'
-    return '\n'    
+        return True
+    if type(prev) in _BLANK_LINE_SURROUNDED or type(current) in _BLANK_LINE_SURROUNDED:
+        return True
+    if _get_category(prev) != _get_category(current):
+        return True
+    return False
 
 
 def _is_directive_or_comment(model: models.RawModel) -> TypeGuard[_DirectiveOrComment]:
     return isinstance(model, get_args(_DirectiveOrComment))
 
 
-@base.formatter(models.File)
-def format_file(file: models.File, context: base.Context) -> Iterator[models.RawTokenModel]:
-    prev = None    
+def _partition(file: models.File, context: base.Context) -> list[_Block]:
+    prev = None
+    blocks: list[_Block] = [[]]
     for child, indented in file.iter_children_formatted():
         if isinstance(child, models.Whitespace | models.Newline):
             continue
         assert _is_directive_or_comment(child)
-        yield models.Whitespace.from_raw_text(_get_spacing(prev, child))
-        yield from base.format(child, context.with_indented(indented))
+        assert not indented
+        if _should_split(prev, child):
+            blocks.append([child])
+        else:
+            blocks[-1].append(child)
         prev = child
-    if prev is not None:
-        yield models.Newline.from_default()
+    return blocks
+
+
+def _render(blocks: list[_Block], context: base.Context) -> Iterator[models.RawTokenModel]:
+    prev = None
+    for block in blocks:
+        if prev:
+            yield models.Newline.from_default()
+        for child in block:
+            yield from base.format(child, context)
+            yield models.Newline.from_default()
+        prev = block
+
+
+@base.formatter(models.File)
+def format_file(file: models.File, context: base.Context) -> Iterator[models.RawTokenModel]:
+    blocks = _partition(file, context)
+    yield from _render(blocks, context)
